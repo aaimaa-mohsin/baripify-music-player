5724a937c80767513e9762e857ab18b44075144c
afnanHashmi123 <afnanhashmi112233@gmail.com>
2021-12-22 01:12:53 +0500

    api authentication and accessing data

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,42 @@
 import Search from './Search';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
-function Body() {
+function Body({spotifyApi}) {
+  const {data:session}=useSession()
+  const { accessToken } = session;
   const [search, setsearch] = useState("");
   const [searchResults,setSearchResults]=useState([]);
+  const [newRelease,setNewRelease]=useState([]);
+
+useEffect(() => {
+  if(!accessToken) return
+  spotifyApi.setAccessToken(accessToken);
+}, [accessToken])
+
+
+//search
+
+
+useEffect(() => {
+  if(!search) return setSearchResults([]);
+  if(!accessToken) return;
+  spotifyApi.searchTracks(search).then((res)=>{
+    setSearchResults(res.Body.tracks.items.map((track)=>{
+      return {
+        id: track.id,
+        artist: track.artist[0].name,
+        title: track.name,
+        uri: track.uri,
+        albumUrl: track.album.images[0].url,
+        popularity: track.popularity,
+      }
+    }))
+  })
+}, [search,accessToken])
+
+console.log(searchResults);
+
     return (
         <section className="bg-black ml-24 py-3 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
          <Search search={search} setSearch={setsearch} />
